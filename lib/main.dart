353f611ff8415e688cbfaa3e290248368ac03987
Pradeep Reddy Kypa <deepu462@gmail.com>
2022-11-05 18:02:12 +0530

    Using Scaffold and Adding An Image in the Body Center

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,19 @@ import 'package:flutter/material.dart';
 //The main function is the starting point for all our Flutter Apps
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('Hello World'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.w3schools.com/howto/img_forest.jpg'),
+          ),
+        ),
       ),
     ),
   );
