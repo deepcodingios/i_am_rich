1500a77d1a8d102826982b516e54cf1b2a02e81e
Pradeep Reddy Kypa <deepu462@gmail.com>
2022-11-05 09:43:18 +0530

    Initial Project Structure

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//The main function is the starting point for all our Flutter Apps
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
