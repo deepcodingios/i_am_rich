906a7543f1fede691ec708a82f6848c5227d896b
Pradeep_Reddy_Kypa <deepu462@gmail.com>
2023-03-19 13:55:53 +0530

    Learning Scaffold and adding Widgets to the Body

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 //The main function is the starting point for all our Flutter Apps
+
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black26,
-        appBar: AppBar(
-          title: const Text('Hello World'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
-          ),
+        home: Scaffold(
+      backgroundColor: Colors.black54,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Center(child: Text('Pradeep Reddy Kypa')),
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
         ),
       ),
-    ),
+    )),
   );
 }
